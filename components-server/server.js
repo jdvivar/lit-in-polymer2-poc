492d7d041855aa3e00190c7f5db1113f9e5521d3
Daniel Vivar <jose.daniel.vivar.personat@ing.com>
2019-09-19 13:59:33 +0200

    WIP server

--- components-server/server.js
+++ components-server/server.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
+const UA = require('@financial-times/polyfill-useragent-normaliser')
+
 const app = require('express')()
 app.use(cors())
 app.listen(8090)
+
+app.use('/components/es5', (request, res, next) => {
+    const userAgentFamily = new UA(request.headers['user-agent']).getFamily()
+    if (userAgentFamily === 'ie') {
+        console.log('Using IE special routes...')
+        app.use('/src', (request, res, next) => {
+            console.log(request.originalUrl)
+            next()
+        })
+    }
+    next()
+})
 
 app.use('/components/es5', express.static(path.join(__dirname,
     'node_modules/components-app/build/es5/node_modules/components-catalog/node_modules'
