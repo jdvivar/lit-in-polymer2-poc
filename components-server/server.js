78d5b5141d72fafe6e789812495a3342a8c72f56
Daniel Vivar <jose.daniel.vivar.personat@ing.com>
2019-09-20 13:43:05 +0200

    FINAL working arch with UNBUNDLED es5 modules for IE11

--- components-server/server.js
+++ components-server/server.js
@@ -1,30 +1,28 @@
 const express = require('express')
 const path = require('path')
 const cors = require('cors')
-const UA = require('@financial-times/polyfill-useragent-normaliser')
 
 const app = express()
 app.use(cors())
 app.listen(8090)
 
-// app.use('*', (request, response, next) => {
-//     const userAgentFamily = new UA(request.headers['user-agent']).getFamily()
-//     if (userAgentFamily === 'ie') {
-//         // app.use('/src', (request, response, next) => {
-            
-//             // })
-//         }
-//         next()
-//     })
-    
+const es5PackagesPath = 'node_modules/components-catalog/build/es5/packages'
+const es6PackagesPath = 'node_modules/components-catalog/build/es6/packages'
+
+app.use('/components',  express.static(path.join(__dirname, es6PackagesPath)))
+app.use('/components/es5', express.static(path.join(__dirname, es5PackagesPath)))
+
+// Serving WRONG IE11 src requests as relative to the specific package src folder
+// In order for this to work, the class definition and name should share the exact same name
 app.use('/src', (request, response, next) => {
     const modulePath = request.path.split('.')[0]
     response.sendFile(path.join(__dirname,`node_modules/components-catalog/build/es5/packages${modulePath}/src${modulePath}.js`))
 })
 
+// Serving WRONG IE11 node_modules requests as relative to the es5/node_modules folder
 app.use('/node_modules', (request, response, next) => {
     response.sendFile(path.join(__dirname,`node_modules/components-catalog/build/es5/node_modules${request.path}`))
 })
 
-app.use('/components/es5', express.static(path.join(__dirname, 'node_modules/components-catalog/build/es5/packages/')))
-app.use('/components', express.static(path.join(__dirname,'node_modules/components-catalog/build/es6/packages')))
+// app.use('/components/es5', express.static(path.join(__dirname, 'node_modules/components-catalog/build/es5/packages/')))
+// app.use('/components', express.static(path.join(__dirname,'node_modules/components-catalog/build/es6/packages')))
