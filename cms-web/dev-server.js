cc86bb2d36c3ca9f9fb51cdc42525906cc72511b
Daniel Vivar <jose.daniel.vivar.personat@ing.com>
2019-10-01 18:21:10 +0200

    Updating cms-web to use ING's own polyfills for ES2015 features

--- cms-web/dev-server.js
+++ cms-web/dev-server.js
@@ -4,9 +4,12 @@ const app = express()
 
 const es5Path = 'node_modules/components-catalog/build/es5'
 const es6Path = 'node_modules/components-catalog/build/es6/packages'
+const port = process.argv[2] || 9999
 
 app.use('/components/es5', express.static(path.join(__dirname, es5Path)))
 app.use('/components', express.static(path.join(__dirname, es6Path)))
 app.use('/', express.static(__dirname))
 
-app.listen(9999)
+app.listen(port)
+
+console.log(`Listening on port ${port} ...`)
