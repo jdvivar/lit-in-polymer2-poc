51463a63ea7774866340f9eee93869b6ad2bbd82
Daniel Vivar <jose.daniel.vivar.personat@ing.com>
2019-10-02 11:15:45 +0200

    Remove unused index.html from components catalog

--- cms-web/webfiles/customElements-define-dedupe.js
+++ cms-web/webfiles/customElements-define-dedupe.js
@@ -1,5 +1,5 @@
 // https://www.npmjs.com/package/@lrnwebcomponents/deduping-fix
-const _customElementsDefine = window.customElements.define || null;
+var _customElementsDefine = window.customElements.define || null;
 window.customElements.define = function (name, cl, conf){
   if (!customElements.get(name)) {
     _customElementsDefine.call(window.customElements, name, cl, conf);
